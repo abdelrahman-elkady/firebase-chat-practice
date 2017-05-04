cf02a6acff52440db8e2e5fea09718fc5888f7d9
kady <elkadyabdelrahman@gmail.com>
2017-05-04 14:44:06 +0200

    Ejecting !

--- src/index.js
+++ src/index.js
@@ -3,10 +3,6 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
 
-import config from './config';
-
-import * as firebase from 'firebase';
-
 ReactDOM.render(
   <App />,
   document.getElementById('root')
