b8bb16cbee4d3bee9117850aecbd92104da5c45a
kady <elkadyabdelrahman@gmail.com>
2017-05-04 14:39:32 +0200

    Debug Travis

--- src/config/index.js
+++ src/config/index.js
@@ -1,7 +1,12 @@
-import firebaseConfig from './private';
-
 const config = {
-  firebase: firebaseConfig
+  firebase: {
+    apiKey: process.env.FIREBASE_API_KEY,
+    authDomain: "fir-chat-2e7c3.firebaseapp.com",
+    databaseURL: "https://fir-chat-2e7c3.firebaseio.com",
+    projectId: "fir-chat-2e7c3",
+    storageBucket: "fir-chat-2e7c3.appspot.com",
+    messagingSenderId: "33831014047"
+  }
 };
 
 export default config;
