a9bcf8a9523f870b2bbd5fd2a6944ea7c81bfb36
kady <elkadyabdelrahman@gmail.com>
2017-05-04 14:22:22 +0200

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import './index.css';
+
+import config from './config';
+
+import * as firebase from 'firebase';
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('root')
+);
